3fa25fd0ded0cfea66fe416b64cd19f29a09f5b6
Silvia Rocha <szalbuque@gmail.com>
2022-12-04 20:23:07 +0000

    altera nome bucket main.tf

--- terraform-exemplo2/main.tf
+++ terraform-exemplo2/main.tf
@@ -6,14 +6,14 @@ terraform {
   }
 
    backend "gcs" {
-    bucket  = "barberoterraform"
+    bucket  = "srochaterraform"
     prefix  = "terraform/state"
   }
   
 }
 
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "bootcampdiome"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
