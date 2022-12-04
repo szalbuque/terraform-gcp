7e798787e631be43ad45177a1ee15b76679fbf8a
Silvia Rocha <szalbuque@gmail.com>
2022-12-04 21:08:34 +0000

    altera o nome da vm

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "cloudbbuildterraform"
   machine_type = "f1-micro"
   tags = ["prod"]
 
